ef38e76554a894092a6b9521ee665b43a11fafca
Maxim Zlotnikov <mzlotnikov@mail.ru>
2022-09-03 11:07:55 +0300

    Example 29

---
+++ Example29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+    // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+    // 6, 1, 33 -> [6, 1, 33]
+
+System.Console.Clear();
+
+int[] arr = new int[8];
+
+Console.WriteLine("Введите 8 (Восемь) целых чисел для создания массива");
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Вы создали массив:");
+Console.Write(string.Join(", ", arr));
